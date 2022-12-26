630f589f1651841e93003f476bf9c9e837ce8c3f
Cibi619 <cibi.mit@gmail.com>
2022-12-26 01:03:12 -0600

    Csharp programs committed..

---
+++ week2/HelloCSharp/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+string myName = "";
+Console.WriteLine("Please enter your name : ");
+try {
+     myName = Console.ReadLine();
+}
+catch (Exception e) {
+    Console.WriteLine(e.Message);
+}
+Console.WriteLine(myName);
