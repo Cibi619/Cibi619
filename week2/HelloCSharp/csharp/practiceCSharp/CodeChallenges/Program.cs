3c45b220b6b4d6b02aa94f1cf67604d4a3c53822
Cibi619 <cibi.mit@gmail.com>
2022-12-29 23:06:14 -0600

    Code file fizzbuzz committed..

---
+++ week2/HelloCSharp/csharp/practiceCSharp/CodeChallenges/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace CSharp {
+    public class Program {
+        public static void Main(String[] args) {
+            int n;
+            Console.WriteLine("Enter the value of n : ");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"n : {n}");
+            string[] answer = new string[n+1];
+            for (int i = 1; i <= n; i++) {
+                if (i % 3 == 0 && i % 5 == 0)
+                    answer[i] = "FizzBuzz";
+                else if (i % 3 == 0)
+                    answer[i] = "Fizz";
+                else if (i % 5 == 0)
+                    answer[i] = "Buzz";
+                else    
+                    answer[i] = i.ToString();
+            }
+
+            // printing the array
+            for (int i = 1; i <= n; i++)
+                Console.Write(answer[i] + " ");
+        }
+    }
+}
